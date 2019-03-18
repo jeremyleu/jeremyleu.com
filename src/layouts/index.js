1257e841e639ee882ff7de2842803bd922efc1ff
Jeremy Leu <jeremyleu@yahoo.com>
2019-03-17 21:03:16 -0700

    add window check

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -6,11 +6,29 @@ import IScroll from 'iscroll';
 
 import './index.scss';
 
-const Layout = ({ children }) => (
-  <ReactIScroll
-    iScroll={IScroll}
-    options={{ mouseWheel: true, scrollbars: false, scrollX: true }}
-  >
+const Layout = ({ children }) =>
+  window ? (
+    <ReactIScroll
+      iScroll={IScroll}
+      options={{ mouseWheel: true, scrollbars: false, scrollX: true }}
+    >
+      <div className="scroll-container">
+        <Helmet
+          title="Jeremy Leu's Portfolio"
+          link={[
+            { rel: 'stylesheet', href: 'https://use.typekit.net/mdx0cer.css' },
+            {
+              rel: 'stylesheet',
+              href:
+                'https://fonts.googleapis.com/css?family=Playfair+Display:700',
+            },
+          ]}
+        />
+        {children()}
+        <div className="line" />
+      </div>
+    </ReactIScroll>
+  ) : (
     <div className="scroll-container">
       <Helmet
         title="Jeremy Leu's Portfolio"
@@ -26,8 +44,7 @@ const Layout = ({ children }) => (
       {children()}
       <div className="line" />
     </div>
-  </ReactIScroll>
-);
+  );
 
 Layout.propTypes = {
   children: PropTypes.func,
