fce0e476541300e41865a0a6b8f419e11947ce34
Jeremy Leu <jeremyleu@yahoo.com>
2019-03-17 22:45:05 -0700

    replace iscroll with better scroll

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,33 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import ReactIScroll from 'react-iscroll';
-import IScroll from 'iscroll';
+import IScroll from 'better-scroll';
 
 import './index.scss';
 
-const Layout = ({ children }) =>
-  window ? (
-    <ReactIScroll
-      iScroll={IScroll}
-      options={{ mouseWheel: true, scrollbars: false, scrollX: true }}
-    >
-      <div className="scroll-container">
-        <Helmet
-          title="Jeremy Leu's Portfolio"
-          link={[
-            { rel: 'stylesheet', href: 'https://use.typekit.net/mdx0cer.css' },
-            {
-              rel: 'stylesheet',
-              href:
-                'https://fonts.googleapis.com/css?family=Playfair+Display:700',
-            },
-          ]}
-        />
-        {children()}
-        <div className="line" />
-      </div>
-    </ReactIScroll>
-  ) : (
+const Layout = ({ children }) => (
+  <ReactIScroll
+    iScroll={IScroll}
+    options={{ mouseWheel: true, scrollbars: false, scrollX: true }}
+  >
     <div className="scroll-container">
       <Helmet
         title="Jeremy Leu's Portfolio"
@@ -44,7 +26,8 @@ const Layout = ({ children }) =>
       {children()}
       <div className="line" />
     </div>
-  );
+  </ReactIScroll>
+);
 
 Layout.propTypes = {
   children: PropTypes.func,
