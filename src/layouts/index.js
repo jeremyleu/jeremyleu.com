06495887a18fac253606a06c5c4981a15b2f46e1
Jeremy Leu <jeremyleu@yahoo.com>
2019-03-17 20:08:33 -0700

    removed query, used layout in index page

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,45 +4,33 @@ import Helmet from 'react-helmet';
 import ReactIScroll from 'react-iscroll';
 import IScroll from 'iscroll';
 
-import Header from '../components/header';
 import './index.scss';
 
-const Layout = ({ children, data }) => (
+const Layout = ({ children }) => (
   <ReactIScroll
     iScroll={IScroll}
     options={{ mouseWheel: true, scrollbars: false, scrollX: true }}
-    onScrollStart={() => { console.log('hi'); }}
   >
     <div className="scroll-container">
       <Helmet
-        title={data.site.siteMetadata.title}
-        meta={[
-          { name: 'description', content: 'Sample' },
-          { name: 'keywords', content: 'sample, something' },
-        ]}
+        title="Jeremy Leu's Portfolio"
         link={[
           { rel: 'stylesheet', href: 'https://use.typekit.net/mdx0cer.css' },
-          { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Playfair+Display:700' },
+          {
+            rel: 'stylesheet',
+            href:
+              'https://fonts.googleapis.com/css?family=Playfair+Display:700',
+          },
         ]}
       />
       {children()}
       <div className="line" />
     </div>
   </ReactIScroll>
-)
+);
 
 Layout.propTypes = {
   children: PropTypes.func,
-}
+};
 
-export default Layout
-
-export const query = graphql`
-  query SiteTitleQuery {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
+export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,18 @@
 import React from 'react';
-import Link from 'gatsby-link';
+
+import Layout from '../layouts';
 
 import './index.scss';
 
 const IndexPage = () => (
-  <div className="components-container">
-    {/* <div className="main-title grid">
-      <div className="row">
-        <div className="cell">J</div>
-        <div className="cell">E</div>
-        <div className="cell">R</div>
+  <Layout>
+    {() => (
+      <div className="components-container">
+        <div className="main-title">Jeremy Leu</div>
+        <div className="sub-title">This site is under construction.</div>
       </div>
-      <div className="row">
-        <div className="cell">E</div>
-        <div className="cell">M</div>
-        <div className="cell">Y</div>
-      </div>
-      <div className="row">
-        <div className="cell">L</div>
-        <div className="cell">E</div>
-        <div className="cell">U</div>
-      </div>
-    </div> */}
-    <div className="main-title">Jeremy Leu</div>
-    <div className="sub-title">This site is under construction.</div>
-  </div>
+    )}
+  </Layout>
 );
 
 export default IndexPage;
